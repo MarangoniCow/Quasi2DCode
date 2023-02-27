9d9d45fb95f183e317abb5350876341f8c7da249
Matthew Sparkes <matthew.sparkes@york.ac.uk>
2023-02-27 10:55:56 +0000

    Generalised viking_script

--- util/Scripts/viking_script.m
+++ util/Scripts/viking_script.m
@@ -1,4 +1,4 @@
-function viking_script(SCRIPTNAME)
+function viking_script(SCRIPTNAME, VAR)
 
 % VIKING_SCRIPT(SCRIPTNAME)
 %
@@ -38,63 +38,59 @@ function viking_script(SCRIPTNAME)
     %   Series non-specific information can be defined here
     % ----------------------------------------------------------------------- %
     
-    % System size: COULD BE OVERWRITTEN BY SERIES SPECIFIC INFORMATION
-    SystemSize              = [1600, 600, 48];
-
-    % x-Axis desired extraction
-    PlaneExtractIdx{1} = 1:1600;
-
-    % y-Axis desired extraction
-    PlaneExtractIdx{2} = 1:600;
-    
     ColloidRadius   = 11.33;  
-    
     RetainVelData   = false;
-    
     saveLocation    = '../Data';
-    
-    
-    % ----------------------------------------------------------------------- %
-    %   Series specific information can be input here
-    % ----------------------------------------------------------------------- %
-    
 
-    
+    switch VAR
+        
+        case XL
+            
+            SystemSize              = [1600, 600, 48];
+            PlaneExtractIdx{1} = 1:1600;
+            PlaneExtractIdx{2} = 1:600;
 
-    % IGNORED
-    switch(seriesNumber)
-        case 1
-          	SystemSize      = [1600, 600, 48];
-        case 2
-            SystemSize      = [384, 256, 56];
-        case 3
-            SystemSize      = [384, 256, 64];
-        case 4
-            SystemSize      = [384, 256, 72];
-        case 5
-            SystemSize      = [384, 256, 80];
-        case 6
-            SystemSize      = [384, 256, 88];
-        case 7
-            SystemSize      = [384, 256, 96];
-        case 8
-            SystemSize      = [384, 256, 104];
-        case 9
-            SystemSize      = [384, 256, 112];
-        case 10
-            SystemSize      = [384, 256, 120];
-        case 11
-            SystemSize      = [384, 256, 128];
-        case 12
-            SystemSize      = [384, 256, 136];
-        case 13
-            SystemSize      = [384, 256, 144];
-        case 14
-            SystemSize      = [384, 256, 152];
-        case 15
-            SystemSize      = [384, 256, 160];
-        otherwise
-		warning('Using default system size')
+        case H
+            SystemSize = [384, 256, 64];
+            PlaneExtractIdx{1} = 1:384;
+            PlaneExtractIdx{2} = 20:236;
+
+        case W
+
+            switch(seriesNumber)
+                case 1
+          	        SystemSize      = [384, 256, 48];
+                case 2
+                    SystemSize      = [384, 256, 56];
+                case 3
+                    SystemSize      = [384, 256, 64];
+                case 4
+                    SystemSize      = [384, 256, 72];
+                case 5
+                    SystemSize      = [384, 256, 80];
+                case 6
+                    SystemSize      = [384, 256, 88];
+                case 7
+                    SystemSize      = [384, 256, 96];
+                case 8
+                    SystemSize      = [384, 256, 104];
+                case 9
+                    SystemSize      = [384, 256, 112];
+                case 10
+                    SystemSize      = [384, 256, 120];
+                case 11
+                    SystemSize      = [384, 256, 128];
+                case 12
+                    SystemSize      = [384, 256, 136];
+                case 13
+                    SystemSize      = [384, 256, 144];
+                case 14
+                    SystemSize      = [384, 256, 152];
+                case 15
+                    SystemSize      = [384, 256, 160];
+                otherwise
+		        warning('Using default system size')
+            end
     end
 
     
